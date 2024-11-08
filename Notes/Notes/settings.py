afa1f637ac49399d75f8b137f32852b98dabada2
Khyati Gupta <160642295+khyatig0206@users.noreply.github.com>
2024-11-08 12:00:48 +0530

    Update settings.py

--- Notes/Notes/settings.py
+++ Notes/Notes/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://my-wishlist-rose.vercel.app",
+    "https://*",
+    
+]
